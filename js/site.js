f83ff3f63b505163aa60ed54ad4e43bf54db9c22
Jamal Gibson <jamal.gibson@rogers.com>
2023-04-14 14:37:40 -0400

    created app logic

--- js/site.js
+++ js/site.js
@@ -1,12 +1,36 @@
-function displayMessage() {
-  let msg = document.getElementById('message').value;
-  //alert(msg);
+function getValues()
+{
+  let userInput = document.getElementById('message').value;
 
-  Swal.fire(
-    {
-        backdrop: false,
-        title: 'App Name',
-        text: msg
-    }
-  );
+  let reversedInput = reverseString(userInput);
+
+  displayString(reversedInput);
 }
+
+
+function reverseString(message)
+{
+  let reversedMessage = '';
+
+  for (i = message.length - 1; i >= 0; i--)
+  {
+    reversedMessage += message[i];
+  }
+
+  return reversedMessage;
+}
+
+
+function displayString(reversedMessage){
+  document.getElementById('msg').textContent = reversedMessage;
+  document.getElementById('alert').classList.remove('d-none');
+}
+
+
+
+
+// Swal.fire({
+//   backdrop: false,
+//   title: "App Name",
+//   text: msg,
+// });
